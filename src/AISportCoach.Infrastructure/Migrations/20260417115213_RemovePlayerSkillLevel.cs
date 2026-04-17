3b36a047062b2957a957eacf31f9c4d65a5ea60a
Dmytro Leonenko <bissleo@gmail.com>
2026-04-17 14:26:55 +0200

    Remove redundant `RemovePlayerSkillLevel` migration file

--- src/AISportCoach.Infrastructure/Migrations/20260417115213_RemovePlayerSkillLevel.cs
+++ src/AISportCoach.Infrastructure/Migrations/20260417115213_RemovePlayerSkillLevel.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,20 +10,13 @@ namespace AISportCoach.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "PlayerSkillLevel",
-                table: "CoachingReports");
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "PlayerSkillLevel",
-                table: "CoachingReports",
-                type: "text",
-                nullable: false,
-                defaultValue: "");
+
         }
     }
 }
