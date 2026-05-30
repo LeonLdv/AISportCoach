19106357a2f280da0ab62c11cc8e6076f3cad28f
Dmytro Leonenko <bissleo@gmail.com>
2026-05-30 09:52:42 +0200

    fix: remove XML doc comments from ReportChunk and IReportChunker

--- src/AISportCoach.Application/Interfaces/IReportChunker.cs
+++ src/AISportCoach.Application/Interfaces/IReportChunker.cs
@@ -3,15 +3,7 @@ using AISportCoach.Domain.Entities;
 
 namespace AISportCoach.Application.Interfaces;
 
-/// <summary>
-/// Chunks a coaching report into semantic units for embedding and storage.
-/// </summary>
 public interface IReportChunker
 {
-    /// <summary>
-    /// Chunks a coaching report into discrete text fragments.
-    /// </summary>
-    /// <param name="report">The coaching report to chunk.</param>
-    /// <returns>A read-only list of report chunks.</returns>
     IReadOnlyList<ReportChunk> Chunk(CoachingReport report);
 }
